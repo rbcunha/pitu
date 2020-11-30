9064961dcea05d5537d41f59271255b6eccdb2e3
rbcunha <barrosdacunha@gmail.com>
2020-11-29 21:02:18 -0300

    implementação da build (produção)

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -5,6 +5,7 @@ import { Container, InputGroup, FormControl, Button, Alert, Spinner } from 'reac
 
 import { ContentContainer, Form, AdsBlock } from './styles';
 import ShortenerService from '../../services/shortenerService';
+import vars from '../../configs/vars';
 
 
 class HomePage extends React.Component {
@@ -76,14 +77,14 @@ class HomePage extends React.Component {
                             <InputGroup className="mb-3"> 
                                 <FormControl
                                     autoFocus={true}
-                                    defaultValue={`https://pitu.tk/${code}`}
+                                    defaultValue={vars.HOST_APP + code}
                                     ref={(input) => this.inputURL = input}
                                 />
                                 <InputGroup.Append>
                                     <Button variant="outline-secondary" onClick={() => this.copyToClipboard()}>Copiar</Button>    
                                 </InputGroup.Append>
                             </InputGroup>
-                            <p>Para acompanhar as estatísticas, acesse https://pitu.tk/{code}</p>
+                            <p>Para acompanhar as estatísticas, acesse {vars.HOST_APP + code}/stats</p>
                           </>  
                         )      
                       )}
